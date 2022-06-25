2a80f66e7a14a30e61b23452c27c59a6ae5e66a1
LuisAngel2k21 <luiscarranza198@yahoo.com>
2022-06-25 17:20:39 -0600

    Ultimo Dia

---
+++ app/src/main/java/com/aplication/tarea4/procesos/Transacciones.java
@@ -0,0 +1,20 @@
+package com.aplication.tarea4.procesos;
+
+public class Transacciones
+{
+
+    public static final String NameDatabase = "DBFOTOS";
+    public static final String tablafotos = "fotos";
+    public static final String id = "id";
+    public static final String nombres = "nombres";
+    public static final String descripcion = "descripcion";
+    public static final String pathImage = "pathImage";
+    public static final String image = "image";
+
+    public static final String CreateTableFotos = "CREATE TABLE tablafotos " +
+            "( id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            " nombres TEXT, descripcion TEXT, pathImage TEXT, "+
+            " pathImage BLOB)";
+    public static final String DropTableFotos = "DROP TABLE IF EXISTS " + tablafotos;
+    public static final String SELECT_ALL_TABLE_PICTURE = "SELECT * FROM " + tablafotos;
+}
